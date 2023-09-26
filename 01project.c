385df3bfe4e19a66fd8027779120b4dcd85d5097
prayag0one4 <142918263+prayag0one4@users.noreply.github.com>
2023-09-26 17:32:53 +0530

    add clear console command .

--- 01project.c
+++ 01project.c
@@ -3,49 +3,60 @@ int main()
 {
     int a, b;
     int c,d;
+    char name[20];
+    printf("Enter your name ");
+    scanf("%s", &name);
+
     printf("select one fruit \napple\nmango\nbanana\norange\ngrapes\n");
     printf("press 1 to start\n");
     scanf("%d", &d);
+      system("clear");
 
 
-    printf("is your fruit available\napple\nmango\norange\ngrapes\npear\nkiwi\nberry\n");
-    printf("enter 1 for YES and 0 for NO\n");
+    printf("Is your fruit available\napple\nmango\norange\ngrapes\npear\nkiwi\nberry\n");
+    
+    printf("Enter 1 for YES and 0 for NO\n");
     scanf("%d", &c);
-
+ 
     if (c == 1)
     {
         a = 1;
     }
-    printf("is your fruit available\nbanana\ndates\nmango\npear\norange\nkiwi\ngrapes\n");
-    printf("enter 1 for YES and 0 for NO\n");
+    system("clear");
+    printf("Is your fruit available\nbanana\ndates\nmango\npear\norange\nkiwi\ngrapes\n");
+    printf("Enter 1 for YES and 0 for NO\n");
     scanf("%d", &c);
     if (c == 1)
     {
         a = a + 1;
     }
-    printf("is your fruit available\ncherry\nbanana\nkiwi\norange\npear\ngrapes\nberry\n");
+      system("clear");
+    printf("Is your fruit available\ncherry\nbanana\nkiwi\norange\npear\ngrapes\nberry\n");
 
-    printf("enter 1 for YES and 0 for NO\n");
+    printf("Enter 1 for YES and 0 for NO\n");
     scanf("%d", &c);
     if (c == 1)
     {
         a = a + 1;
     }
-    printf("is your fruit available\nmelon\nbanana\ndates\nberry\npear\norange\ngrapes\nplum\n");
-    printf("enter 1 for YES and 0 for NO\n");
+      system("clear");
+    printf("Is your fruit available\nmelon\nbanana\ndates\nberry\npear\norange\ngrapes\nplum\n");
+    printf("Enter 1 for YES and 0 for NO\n");
     scanf("%d", &c);
     if (c == 1)
     {
         a = a + 1;
     }
-    printf("is your fruit available\npear\ndates\nmelon\norange\ncherry\nberry\nplum\n");
-    printf("enter 1 for YES and 0 for NO\n");
+      system("clear");
+    printf("Is your fruit available\npear\ndates\nmelon\norange\ncherry\nberry\nplum\n");
+    printf("Enter 1 for YES and 0 for NO\n");
     scanf("%d", &c);
     if (c == 1)
     {
         a = a + 1;
     }
-
+      system("clear");
+      printf("\nHey %s\n\n", name);
     if(a==1){
         printf("your fruit is apple");
     }
